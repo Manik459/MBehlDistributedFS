fa16a6f534841487f72a6772fb4e5056ee12ed8c
Manik Behl <behlmanik459@gmail.com>
2017-12-06 03:20:02 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,13 +4,13 @@ import os.path
 def find_file(filename):
 	print("the file we are looking for is ", filename)
 
-	if filename in  os.listdir("DB_Files/"):
+	if filename in  os.listdir("DBM_File/"):
 		print("file exists on db")
-		file_path= "DB_files/" + filename
+		file_path= "DBM_file/" + filename
 		return file_path
 	else:
 		print("file does not exist on db. creating file")
-		file_path= "DB_files/" + filename
+		file_path= "DBM_file/" + filename
 		f=open(file_path,"w+")
 		f.close()
 		return file_path
