8cc00a903adec7832d74a2b39ed454460335c5d5
Manik Behl <behlmanik459@gmail.com>
2017-12-06 03:05:37 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,16 @@
+import os
+import os.path
+
+def find_file(filename):
+	print("the file we are looking for is ", filename)
+
+	if filename in  os.listdir("DB_Files/"):
+		print("file exists on db")
+		file_path= "DB_files/" + filename
+		return file_path
+	else:
+		print("file does not exist on db. creating file")
+		file_path= "DB_files/" + filename
+		f=open(file_path,"w+")
+		f.close()
+		return file_path
